0dcac57f3755d0381a885af21b584b3091a06fa7
CadavidR <sebastian.cadavid9412@udea.edu.co>
2021-09-22 23:52:42 -0500

    design body

--- lib/src/details/components/body.dart
+++ lib/src/details/components/body.dart
@@ -2,12 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:oldwave/src/models/product.dart';
 
 class Body extends StatelessWidget {
-  final Product product;
+  //final Product product;
+  //const Body({Key key, this.product}): super(key:key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: size.height,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  margin:EdgeInsets.only(top:size.height *0.3),
+                  height: 500,
+                  decoration:BoxDecoration(
+                    color: Colors.green,
+                  ),
+                ),
+                Column(
+                  children: <Widget>[
+                    Text("nombre del producto con recursos ui")
+                  ],
+                )
+              ],
+              ),
+          )
+        ],
+        ),
     );
   }
 }
