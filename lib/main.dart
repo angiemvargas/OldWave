e3fa79d012e8da2fea230bfa37308344ac4ebabe
sescobar99 <santi.e.c@hotmail.com>
2021-09-24 20:40:40 -0500

    Checkout page y notifier provider test

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+// import 'package:flutter_cart/flutter_cart.dart';
+// import 'package:flutter_cart/model/cart_response_wrapper.dart';
+import 'package:oldwave/src/blocs/provider/cart_provider.dart';
+import 'package:oldwave/src/routes/routes.dart';
 import 'package:oldwave/src/ui/home.dart';
 
 void main() {
   runApp(MyApp());
+
+  // var cart = FlutterCart();
+
+  // cart.addToCart(productId: '001', unitPrice: 100);
+  // cart.addToCart(productId: '002', unitPrice: 200);
+  // dynamic aux = cart.addToCart(productId: '003', unitPrice: 350);
+  // print(cart);
+  // print(cart.cartItem);
+  // print(cart.message);
+  // print(cart.runtimeType);
+  // print(cart.toString());
+  // print(cart.getTotalAmount());
+  // // print(cart.getSpecificItemFromCart(cartId));
+  // print(cart.getCartItemCount());
+  // print(cart.decrementItemFromCart(1));
+  // print(cart.incrementItemToCart(2));
+  // print(cart);
+  // print(aux);
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'OldWave',
-      home: Home(),
-      theme: ThemeData(
-        fontFamily: 'Poppins',
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CartProvider()),
+      ],
+      child: MaterialApp(
+        title: 'OldWave',
+        home: Home(),
+        routes: getApplicationRoutes(),
+        theme: ThemeData(
+          fontFamily: 'Poppins',
+        ),
       ),
     );
   }
