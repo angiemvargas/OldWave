07f1c151dacfe333ac8f5b82f9d7dd86b7f1e553
CadavidR <sebastian.cadavid9412@udea.edu.co>
2021-09-24 19:40:39 -0500

    refactor detail-product-response

--- lib/src/resources/dtos/detail_product_response.dart
+++ lib/src/resources/dtos/detail_product_response.dart
@@ -1,21 +1,39 @@
 import 'package:oldwave/src/resources/dtos/product_response.dart';
 
 class DetailProductResponse {
-  SellerDetailResponse seller = SellerDetailResponse(0, '');
-
+  
   int id = 0;
   String name = '';
+  String city = '';
+  String brand = '';
+  String thumbnail= '';
+  String rating ="";
+  String price = '';
+  SellerDetailResponse seller = SellerDetailResponse(0, '');
+  List<ProductResponse> images = <ProductResponse>[];
 
   DetailProductResponse(
     this.id,
     this.name,
-    this.seller
+    this.city,
+    this.brand,
+    this.thumbnail,
+    this.rating,
+    this.price,
+    this.seller,
+    this.images
   );
 
   DetailProductResponse.fromJson(Map<String, dynamic> json){
     id = json['id'];
     name = json['name'];
-    seller = new SellerDetailResponse.fromJson(json['seller']); 
+    city= json['city'];
+    brand = json['brand'];
+    thumbnail = json['thumbnail'];
+    rating= json['rating'];
+    price = json['price'];
+    seller = new SellerDetailResponse.fromJson(json['seller']);
+    json['images'].forEach((v) => images.add(new ProductResponse.fromJson(v))); 
   }
 }
 
