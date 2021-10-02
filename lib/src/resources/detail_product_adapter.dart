c84f54cc90d00e85310615ec3d30650249083734
sescobar99 <santi.e.c@hotmail.com>
2021-10-01 22:03:46 -0500

    Desactivar solicitud a servicio de Spring

--- lib/src/resources/detail_product_adapter.dart
+++ lib/src/resources/detail_product_adapter.dart
@@ -5,31 +5,29 @@ import 'package:oldwave/src/network_provider/constant.dart';
 import 'package:oldwave/src/network_provider/detail_product_service.dart';
 
 class DetailProductAdapter implements DetailProductRepository {
-
   final DetailProductService service = DetailProductService();
 
   @override
-  Future<DetailProduct> getDetailProductById(String idProduct,  String idSeller) async {
-
-    DetailProduct product = DetailProduct(0, '', '', List.empty(), City(0, ''), 0.0, 0.0, '', SellerWithLogo(0, '', ''));
+  Future<DetailProduct> getDetailProductById(
+      String idProduct, String idSeller) async {
+    DetailProduct product = DetailProduct(0, '', '', List.empty(), City(0, ''),
+        0.0, 0.0, '', SellerWithLogo(0, '', ''));
 
     // asumimos que fast api tiene id de seller 1, pendiente de confirmar
-    if(idSeller == '1'){
-      product = await service.getDetailProduct(Constant.url_back_fast_api, idProduct);
+    if (idSeller == '1') {
+      product =
+          await service.getDetailProduct(Constant.url_back_fast_api, idProduct);
     }
     // asumimos que spring tiene id de seller 2, pendiente de confirmar
-    if(idSeller == '2'){
-      product = await service.getDetailProduct(Constant.url_back_spring_boot, idProduct);
-    }
+    // if(idSeller == '2'){
+    //   product = await service.getDetailProduct(Constant.url_back_spring_boot, idProduct);
+    // }
     // asumimos que flask tiene id de seller 3, pendiente de confirmar
-    if(idSeller == '3'){
-      product = await service.getDetailProduct(Constant.url_back_flask, idProduct);
+    if (idSeller == '3') {
+      product =
+          await service.getDetailProduct(Constant.url_back_flask, idProduct);
     }
 
     return product;
-
   }
-
-
-
 }
