842d494476048929fac3f6ec8497c1e340da6645
sescobar99 <santi.e.c@hotmail.com>
2021-10-01 22:01:27 -0500

    Desactivar DebugBanner

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:oldwave/common/theme.dart';
-import 'package:oldwave/src/ui/widgets/products.dart';
 import 'package:provider/provider.dart';
 import 'package:oldwave/src/blocs/provider/cart_provider.dart';
 import 'package:oldwave/src/routes/routes.dart';
 import 'package:oldwave/src/ui/home.dart';
-import 'package:oldwave/src/ui/product_list.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,6 +18,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         title: 'OldWave',
+        debugShowCheckedModeBanner: false,
         home: Home(),
         routes: getApplicationRoutes(),
         theme: appTheme,
