c8f607d9283045836ecd6ec62c905584771c8b65
sescobar99 <santi.e.c@hotmail.com>
2021-10-14 21:58:00 -0500

    Actualizar endpoint fastapi, renderizar logo seller

--- lib/src/ui/widgets/details/components/cart_counter.dart
+++ lib/src/ui/widgets/details/components/cart_counter.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:oldwave/src/models/detail_product.dart';
 import 'package:oldwave/src/ui/material_ui/constants.dart';
 import 'package:oldwave/src/ui/size_config.dart';
@@ -12,15 +12,14 @@ class CartCounter extends StatefulWidget {
   Counter numItem;
   CartCounter({required this.product, required this.numItem});
   @override
-  _CartCounterState createState() => _CartCounterState (); 
+  _CartCounterState createState() => _CartCounterState();
 }
-  
-class _CartCounterState extends State<CartCounter>{
-  
+
+class _CartCounterState extends State<CartCounter> {
   @override
   Widget build(BuildContext context) {
     Counter numItem = widget.numItem;
-    var product= widget.product;
+    var product = widget.product;
     return Row(
       children: [
         SizedBox(
@@ -29,18 +28,16 @@ class _CartCounterState extends State<CartCounter>{
             aspectRatio: 0.88,
             child: GestureDetector(
               onTap: () {
-                // dirigir a detalle?
+                // Dirigir a detalle?
               },
               child: Container(
                 padding: EdgeInsets.all(getProportionateScreenWidth(10)),
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(15),
-                  
                 ),
-                child: Image.network(
-                  product.seller.logo,
-                ),
+                child: SvgPicture.network(product.seller.logo,
+                    height: 10, width: 10),
               ),
             ),
           ),
@@ -52,47 +49,49 @@ class _CartCounterState extends State<CartCounter>{
             Container(
               width: 120,
               child: Text(
-                'vendedor : '+product.seller.name,
-                style: TextStyle(color: Colors.black, fontSize: 16,fontWeight: FontWeight.bold),
+                'Vendedor : ' + product.seller.name,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
                 overflow: TextOverflow.fade,
                 maxLines: 3,
               ),
             ),
             SizedBox(height: 10),
-            
           ],
         ),
         SizedBox(width: 10),
         RoundedIconBtn(
-            icon: Icons.remove,
-            showShadow: true,
-            press: () {
-              if(numItem.numItem > 1){
-                setState((){
+          icon: Icons.remove,
+          showShadow: true,
+          press: () {
+            if (numItem.numItem > 1) {
+              setState(() {
                 numItem.down();
               });
             }
-            },
-          ),
+          },
+        ),
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: oldWaveDefaultPaddin / 2),
+          padding:
+              const EdgeInsets.symmetric(horizontal: oldWaveDefaultPaddin / 2),
           child: Text(
             numItem.numItem.toString().padLeft(2, "0"),
             style: Theme.of(context).textTheme.headline6,
           ),
         ),
         RoundedIconBtn(
-            icon: Icons.add,
-            showShadow: true,
-            press: () {
-              setState((){
-              numItem.up();});
-            },
-          ),
+          icon: Icons.add,
+          showShadow: true,
+          press: () {
+            setState(() {
+              numItem.up();
+            });
+          },
+        ),
       ],
       mainAxisAlignment: MainAxisAlignment.spaceAround,
     );
   }
-
 }
-
