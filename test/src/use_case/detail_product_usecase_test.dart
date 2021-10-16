e0b118309166be8853ee0255ec78ba615114eeb2
sescobar99 <santi.e.c@hotmail.com>
2021-10-16 08:07:42 -0500

    fallo intencional de prueba

--- test/src/use_case/detail_product_usecase_test.dart
+++ test/src/use_case/detail_product_usecase_test.dart
@@ -7,21 +7,22 @@ import 'package:oldwave/src/models/repository/detail_product_repository.dart';
 import 'package:oldwave/src/use_case/detail_product_usecase.dart';
 
 import 'detail_product_usecase_test.mocks.dart';
-@GenerateMocks([DetailProductRepository])
-void main(){
 
+@GenerateMocks([DetailProductRepository])
+void main() {
   test('llamado del servicio desde el usecase', () async {
     final repository = MockDetailProductRepository();
     final useCase = DetailProductUseCase();
     int idSeller = 1;
     int idProduct = 1;
 
-    when(repository.getDetailProductById(idProduct, idSeller))
-    .thenAnswer((_) async => DetailProduct(1, '', '', List.empty(), City(1, ''), 0.0, 0.0, '', SellerWithLogo(1, '', '')));
+    when(repository.getDetailProductById(idProduct, idSeller)).thenAnswer(
+        (_) async => DetailProduct(1, '', '', List.empty(), City(1, ''), 0.0,
+            0.0, '', SellerWithLogo(1, '', '')));
 
-    Future<DetailProduct> actual = useCase.getDetailProductById(idProduct, idSeller);
+    Future<DetailProduct> actual =
+        useCase.getDetailProductById(idProduct, idSeller);
 
-    expect(await actual, isNotNull);
-
+    expect(await actual, isNull);
   });
 }
